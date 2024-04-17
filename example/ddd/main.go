3ed722747563021793608c2a931eff0698cade19
Isaque Veras <isaqueveras@alu.ufc.br>
2024-04-16 23:12:47 -0300

    feat(ddd): http client to get info ddd

---
+++ example/ddd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/isaqueveras/brasilapi-go"
+	"github.com/isaqueveras/brasilapi-go/ddd"
+	"github.com/isaqueveras/juazeiro"
+)
+
+func main() {
+	conn, err := juazeiro.NewClient(brasilapi.ServerClient)
+	if err != nil {
+		panic(err)
+	}
+
+	client := ddd.NewDddClient(conn)
+
+	var zipcode *ddd.Response
+	if zipcode, err = client.Obtain(context.Background(), &ddd.Identifier{
+		Ddd: pointer(int64(88)),
+	}); err != nil {
+		panic(err)
+	}
+
+	str, _ := json.Marshal(zipcode)
+	fmt.Println(string(str))
+}
+
+func pointer[t any](value t) *t {
+	return &value
+}
