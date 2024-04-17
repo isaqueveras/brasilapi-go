c8bdf4019bd49ca597b7c45b11ad0bc6578fe80a
Isaque Veras <isaqueveras@alu.ufc.br>
2024-04-16 22:48:04 -0300

    feat(cep): http client to get zip code

---
+++ example/cep/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/isaqueveras/brasilapi-go"
+	"github.com/isaqueveras/brasilapi-go/cep"
+	"github.com/isaqueveras/juazeiro"
+)
+
+func main() {
+	conn, err := juazeiro.NewClient(brasilapi.ServerClient)
+	if err != nil {
+		panic(err)
+	}
+
+	client := cep.NewCepClient(conn)
+
+	var zipcode *cep.ZipCode
+	if zipcode, err = client.GetZipCode(context.Background(), &cep.Identifier{
+		Cep: pointer("63900-193"),
+	}); err != nil {
+		panic(err)
+	}
+
+	str, _ := json.Marshal(zipcode)
+	fmt.Println(string(str))
+}
+
+func pointer[t any](value t) *t {
+	return &value
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/isaqueveras/brasilapi-go
 
-go 1.16
+go 1.18
+
+require github.com/isaqueveras/juazeiro v0.0.0-20240416010309-68d04496488e
